0b94a6023a1289e7ffada03df9e7049bd30862a0
liamlevesque <liam.levesque@feralcatlabs.com>
2017-04-11 09:37:33 -0700

    initial commit

---
+++ assets/js/_bidding.js
@@ -0,0 +1,77 @@
+$(function(){
+
+
+});
+
+const dataObject = {
+	getOtherSalesVisible: false,
+	dataCenterSwitchVisible: false,
+	deleteEventVisible: false,
+	addDisplayVisible: false,
+	displayList: [],
+	tempDisplay: {},
+	saveDisplayAllowed: false,
+	historyExpanded: false,
+	CCYSettingsVisible: false,
+};
+
+const controller = {
+	toggleGetOtherSales: function(e){
+		dataObject.getOtherSalesVisible = !dataObject.getOtherSalesVisible;
+	},
+	toggleDataCenterSwitch: function(e){
+		dataObject.dataCenterSwitchVisible = !dataObject.dataCenterSwitchVisible;	
+	},
+	toggleDeleteEventVisible: function(e){
+		dataObject.deleteEventVisible = !dataObject.deleteEventVisible;
+	},
+	toggleAddDisplayVisible: function(e){
+		dataObject.addDisplayVisible = !dataObject.addDisplayVisible;
+		dataObject.tempDisplay = null;
+		dataObject.tempDisplay = {};
+	},
+	toggleHistoryExpanded: function(){
+		dataObject.historyExpanded = !dataObject.historyExpanded;
+	},
+	toggleCCYSettingsVisible: function(){
+		dataObject.CCYSettingsVisible = !dataObject.CCYSettingsVisible;
+	},
+
+	goToEventList: function(){
+		window.location = "/";
+	},
+
+	//DISPLAY SETUP CONTROLS
+	setDisplayType: function(e){
+		dataObject.tempDisplay.type = $(e.currentTarget).data('type');
+	},
+	updateDisplayName: function(e){
+		if($(e.currentTarget).val().length > 0) dataObject.saveDisplayAllowed = true;
+		else dataObject.saveDisplayAllowed = false;
+	},
+	createDisplay: function(e){
+		dataObject.displayList.push(dataObject.tempDisplay);
+		controller.toggleAddDisplayVisible();
+	},
+	editDisplay: function(e,context){
+		console.log(context);
+		dataObject.addDisplayVisible = true;
+		dataObject.tempDisplay.type = context.display.type;
+		dataObject.tempDisplay.name = context.display.name;
+	}
+};
+
+const focusFirstInput = function(container){
+	container.find('input').first().focus();
+}
+
+
+rivets.bind($('.js--data-context'),{
+	dataObject: dataObject,
+	controller: controller
+});
+
+
+
+
+
